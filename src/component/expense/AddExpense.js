73a504b5ae62345513625feb6c0898ebab222742
rohit <rohitgupta2432@gmail.com>
2021-08-25 23:00:04 +0530

    expense design in progress

--- src/component/expense/AddExpense.js
+++ src/component/expense/AddExpense.js
@@ -2,31 +2,35 @@ import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
 import './AddExpense.css'
+import { FcPlus } from 'react-icons/fc'
+
 const AddExpense = () => {
     const [startDate, setStartDate] = useState(new Date())
     return (
         <>
             <div className="main_container">
                 <div className="expense_date form-group">
-                    <lable className="date">
+                    {/* <lable className="date">
                         Date :
-                    </lable>
-                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                    </lable> */}
+                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} placeholder="date"/>
                 </div>
                 <div className="category_expense">
-                    <label htmlFor="expense">
+                    {/* <label htmlFor="expense">
                         Category :
-                    </label>
+                    </label> */}
                     <select name="" id="">
+                        <option value="">Select Category</option>
                         <option value="">Food</option>
                         <option value="">HouseHold</option>
                         <option value="">grociery</option>
                     </select>
+                    <FcPlus className="add_category"/>
                 </div>
                 <div className="amount_expense">
-                    <label htmlFor="" className="amount">
+                    {/* <label htmlFor="" className="amount">
                         Amount :
-                     </label>
+                     </label> */}
                     <input type="number" placeholder="Amount" />
                 </div>
                 <input type="submit" value="submit" />
