ef93e6a60720771575fb2cc682e961dffc7d3f15
rohit <rohitgupta2432@gmail.com>
2021-08-27 08:55:44 +0530

    category create page

--- src/component/catgory/Category.js
+++ src/component/catgory/Category.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Category.css'
 import { FaAngleLeft } from 'react-icons/fa'
 import { useHistory } from 'react-router-dom'
+import Axios from 'axios';
+import { ENV } from '../environment/EnvrUrl'
 
 const Category = () => {
     const history = useHistory();
-   
+    const [categoryObj, setCategoryObj] = useState({
+        name: ""
+    });
+
+    const handleInput = (e) => {
+        setCategoryObj({ ...categoryObj, [e.target.name]: e.target.value });
+    }
+
+    const createCategory = (e) => {
+        e.preventDefault();
+
+        console.log(categoryObj);
+
+        Axios.post(ENV.URL + 'category', categoryObj)
+            .then((res) => {
+                console.log(res);
+            }).catch((err) => {
+                console.log(err);
+            })
+
+    }
+
     return (
         <>
-            <form action="">
+            <form action="" onSubmit={createCategory}>
                 <div className="container_cate">
-                <FaAngleLeft className="go_back" onClick={history.goBack}/>
-                    <input type="text" placeholder="Category" />
+                    <FaAngleLeft className="go_back" onClick={history.goBack} />
+                    <input type="text" placeholder="Category" id="name" name="name" value={categoryObj.name}
+                        onChange={handleInput} />
                     <hr />
                     <div className="form_submit">
                         <input type="submit" value="submit" className="category_submit" />
--- src/component/expense/AddExpense.js
+++ src/component/expense/AddExpense.js
@@ -1,31 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
 import './AddExpense.css'
 import { FcPlus } from 'react-icons/fc'
 import { useHistory } from 'react-router-dom'
 import { FaAngleLeft } from 'react-icons/fa'
+import { ENV } from '../environment/EnvrUrl'
+import Axios from 'axios';
+
 
 const AddExpense = () => {
     const [startDate, setStartDate] = useState(new Date())
+    const [category, setCategory] = useState([]);
     const history = useHistory();
+
     const addCategory = () => {
-        console.log('add category');
         history.push("/category");
     }
+
+
+    useEffect(() => {
+        Axios.get(ENV.URL + 'category')
+            .then((response) => {
+                //console.log(response.data);
+                setCategory(response.data);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }, []);
+
     return (
         <>
             <div className="main_container">
-            <FaAngleLeft className="go_back" onClick={history.goBack}/>
+                <FaAngleLeft className="go_back" onClick={history.goBack} />
                 <div className="expense_date form-group">
                     <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
                 </div>
                 <div className="category_expense">
-                    <select name="" id="">
+                    <select>
                         <option value="">Select Category</option>
-                        <option value="">Food</option>
-                        <option value="">HouseHold</option>
-                        <option value="">grociery</option>
+                        {
+                            category.map((value) => {
+                                return <option key={value.id} value={value.id}>{value.name}</option>
+                            })
+                        }
                     </select>
                     <FcPlus className="add_category" onClick={addCategory} />
                 </div>
@@ -33,7 +51,7 @@ const AddExpense = () => {
 
                     <input type="number" placeholder="Amount" />
                 </div>
-                <input type="submit" value="submit" className="expense_submit"/>
+                <input type="submit" value="submit" className="expense_submit" />
             </div>
         </>
     )
